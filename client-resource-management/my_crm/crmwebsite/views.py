b21abc59f85fe2d878d97240224a9c153c440cc7
Viswa v <viswamalar2004@gmail.com>
2025-11-01 23:46:07 +0530

    Created my login and logout functionality ✅

--- client-resource-management/my_crm/crmwebsite/views.py
+++ client-resource-management/my_crm/crmwebsite/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-  return render(request, 'home.html',{})
+    if request.method == 'POST': 
+        username = request.POST['username']  
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in successfully.")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password.")  
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)  
+    messages.success(request, "You have been logged out successfully.")
+    return redirect('home')

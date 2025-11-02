3f2d730a47a15ea1bc60d53dbec45d67775c902a
Viswa v <viswamalar2004@gmail.com>
2025-11-02 14:06:26 +0530

    created models

--- client-resource-management/my_crm/crmwebsite/models.py
+++ client-resource-management/my_crm/crmwebsite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return (f"{self.name}")
